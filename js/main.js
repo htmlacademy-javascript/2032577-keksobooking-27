8db26bd5658db64f01c28c4f64ad6937a1d17c25
Siestaa <lex.qz97@gmail.com>
2022-11-05 00:53:56 +0500

    add bug map

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-import './rent-list.js';
-import { nonActive } from './form-active.js';
+import {initMap, setOnMapLoad, setOnmainPinMove, setAdPins} from './map.js';
+import {nonActive} from './form-active.js';
+import {getRentList} from './data.js';
 import './form-valid.js';
+import './map.js';
 
 // nonActive();
+
+const START_COORDINATE = {
+  lat: 35.66023,
+  lng: 139.73007
+};
+
+const offers = getRentList();
+
+initMap(START_COORDINATE);
 
